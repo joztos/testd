3750e65c8e6b0fb684d164398be659034b3f6c39
Your Name <you@example.com>
2023-05-13 11:46:34 -0600

    primer modificacion

--- example/views.py
+++ example/views.py
@@ -1,12 +1,18 @@
 # example/views.py
 from datetime import datetime
-
 from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
     html = f'''
     <html>
+        <style>
+            body {{
+                background-image: url('https://i.im.ge/2023/05/13/UbjYym.como-disfrutar-del-sexo-con-condon-1550x.webp');
+                background-repeat: no-repeat;
+                background-size: cover;
+            }}
+        </style>
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
